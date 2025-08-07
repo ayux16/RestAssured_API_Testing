6911332db36acd943f4acddac9eabf6d4dbdfe71
Ayush <ayushx16@gmail.com>
2025-08-07 13:46:36 +0530

    Rest_Assured_Api_Testing

--- src/main/java/org/Prototype/files/Token/AuthorizationToken.java
+++ src/main/java/org/Prototype/files/Token/AuthorizationToken.java
@@ -2,10 +2,10 @@ package org.Prototype.files.Token;
 
 public class AuthorizationToken {
 
-    public static String AuthenticationBearerBASE64Token(){
-        return "Basic YXl1c2gudmVybWF4MTZAZ21haWwuY29tOkFUQVRUM3hGZkdGMC0wOXBuQ3" +
-                "V1QUxFQ2tJQVdkbl8zSE9WRXNxNHZXUVdKQzFqRFBZWURGc0t2NHlycE8zMVNXeHQtb" +
-                "1lvTVQzYlhjRWE5LVhRdXFvelBMeTB3VVdxYjlRcXZQNHBXUnYzOWlJVUduRnRYTng1VWdwclgxeWFPUHp" +
-                "EbnZGa1h6YkRBYzd4UGg0N2ZBcXNod1gyUFdTQUhmZU91SVljSFU2c3oxVHFMVmMwSmx4OD0wOUNBOTBFMg==";
-    }
+//    public static String AuthenticationBearerBASE64Token(){
+//        return "Basic YXl1c2gudmVybWF4MTZAZ21haWwuY29tOkFUQVRUM3hGZkdGMC0wOXBuQ3" +
+//                "V1QUxFQ2tJQVdkbl8zSE9WRXNxNHZXUVdKQzFqRFBZWURGc0t2NHlycE8zMVNXeHQtb" +
+//                "1lvTVQzYlhjRWE5LVhRdXFvelBMeTB3VVdxYjlRcXZQNHBXUnYzOWlJVUduRnRYTng1VWdwclgxeWFPUHp" +
+//                "EbnZGa1h6YkRBYzd4UGg0N2ZBcXNod1gyUFdTQUhmZU91SVljSFU2c3oxVHFMVmMwSmx4OD0wOUNBOTBFMg==";
+//    }
 }
